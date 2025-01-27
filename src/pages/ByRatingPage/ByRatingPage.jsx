736dd2fd3ec1bf316a18d817b7a8bf2580c72916
Yevhen Maslov <yevheniimaslov@gmail.com>
2025-01-27 22:47:00 +0200

    fix dep

--- src/pages/ByRatingPage/ByRatingPage.jsx
+++ src/pages/ByRatingPage/ByRatingPage.jsx
@@ -10,7 +10,7 @@ const ByRatingPage = () => {
     const getMoviesByRating = async () => {
       try {
         const moviesByRating = await fetchMoviesByRating();
-        setMovies(moviesByRating || []);
+        setMovies(moviesByRating.results || []);
       } catch (error) {
         console.error("Failed to fetch movies by rating", error);
       }
